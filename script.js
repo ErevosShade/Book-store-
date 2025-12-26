634961b73aaeba11fd95f4809e86a74d203926b0
ErevosShade <mysilentblade.com>
2025-12-26 19:11:03 +0530

    fixing line inconsistency in login box

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const closeBtn = document.getElementById("closeModal");
 
 // OPEN from navbar Sign In
 openBtn.addEventListener("click", () => {
-  modal.style.display = "flex";
+    modal.style.display = "flex";
 });
 
 // CLOSE (X button)
 closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+    modal.style.display = "none";
 });
 
 // CLOSE on outside click
 modal.addEventListener("click", (e) => {
-  if (e.target === modal) modal.style.display = "none";
+    if (e.target === modal) modal.style.display = "none";
 });
 
 // CLOSE on ESC
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") modal.style.display = "none";
+    if (e.key === "Escape") modal.style.display = "none";
 });
 
